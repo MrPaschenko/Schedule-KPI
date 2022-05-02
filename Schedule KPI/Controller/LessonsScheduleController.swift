3d2d7b7589ed3d4fb091b1b63075c37b89967b23
mrpaschenko <paschenkod2@gmail.com>
2022-05-02 17:12:32 +0300

    Add TableView for first week

--- Schedule KPI/Controller/LessonsScheduleController.swift
+++ Schedule KPI/Controller/LessonsScheduleController.swift
@@ -3,20 +3,80 @@ import UIKit
 
 class LessonsScheduleController: UITableViewController {
     var scheduleManager = ScheduleManager()
+    var scheduleWeek = [ScheduleDay]()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         scheduleManager.delegate = self
-        scheduleManager.getSchedule(groupCode: "Іп-01")
+        scheduleManager.getSchedule(groupCode: "іп-04")
     }
 }
 
+//MARK: - TableView
+extension LessonsScheduleController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return scheduleWeek.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scheduleWeek[section].pairs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PairCell", for: indexPath)
+        
+        var configuration = cell.defaultContentConfiguration()
+        let pair = scheduleWeek[indexPath.section].pairs[indexPath.row]
+        configuration.text = pair.name
+        configuration.secondaryText = pair.time
+        configuration.secondaryTextProperties.color = .systemBlue
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: scheduleWeek[indexPath.section].pairs[indexPath.row].name,
+                                      message: "\(scheduleWeek[indexPath.section].pairs[indexPath.row].teacherName)\n\(scheduleWeek[indexPath.section].pairs[indexPath.row].place)",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Добре", style: .default))
+        present(alert, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch scheduleWeek[section].day {
+        case "Пн":
+            return scheduleWeek[section].pairs.isEmpty ? nil : "Понеділок"
+        case "Вв":
+            return scheduleWeek[section].pairs.isEmpty ? nil : "Вівторок"
+        case "Ср":
+            return scheduleWeek[section].pairs.isEmpty ? nil : "Середа"
+        case "Чт":
+            return scheduleWeek[section].pairs.isEmpty ? nil : "Четвер"
+        case "Пт":
+            return scheduleWeek[section].pairs.isEmpty ? nil : "Пʼятниця"
+        case "Сб":
+            return scheduleWeek[section].pairs.isEmpty ? nil : "Субота"
+        default:
+            return scheduleWeek[section].day
+        }
+
+        return scheduleWeek[section].day
+    }
+}
+
+//MARK: - Networking
 extension LessonsScheduleController: ScheduleManagerDelegate {
     func didUpdateSchedule(_ scheduleManager: ScheduleManager, schedule: Schedule) {
-        print("schedule is received")
-        
         let firstWeekSchedule = sortPairs(in: schedule.data.scheduleFirstWeek)
         let secondWeekSchedule = sortPairs(in: schedule.data.scheduleSecondWeek)
-        print(firstWeekSchedule[0].pairs[0])
+        scheduleWeek = firstWeekSchedule
+        
+        DispatchQueue.main.async {
+            self.navigationItem.title = schedule.data.groupCode
+            self.tableView.reloadData()
+        }
     }
     
     func didFailWithError(error: Error) {
